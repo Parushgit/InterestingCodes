32773788f57465956d0004026438434235caaa38
Parush Garg <parush.theultimate@gmail.com>
2018-09-23 12:52:41 -0400

    Added solution to OneEditDistance problem

---
+++ oneEditDistance.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<stdio.h>
+#include<string>
+
+using namespace std;
+
+bool isOneEditDistancePossible(string a, string b) {
+    int diff, m, n, i, j;
+    m = a.length();
+    n = b.length();
+    int count = 0;
+
+    diff = abs(m-n);
+    if(diff > 1)
+        return false;
+    
+    while(i < m && j < n) {
+        if(a[i] != b[j]) {
+            if(count > 1)
+                return true;
+            else if(m > n)
+                i++;
+            else if(n < m)
+                j++;
+            else {
+                i++;
+                j++;
+            }
+            count++; 
+        }
+        i++;
+        j++;
+    }
+
+    if(i < m || j < n)
+        count++;
+    
+    return count == 1;
+}
+int main()
+{
+    string a = "geekss";
+    string b = "geaks";
+    cout<<isOneEditDistancePossible(a, b)<<endl;
+    return 0;
+}
